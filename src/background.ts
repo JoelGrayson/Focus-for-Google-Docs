a6b810c8444e4bc3292f1a3c78f95a63483d7ac5
Joel Grayson <joel@joelgrayson.com>
2024-12-28 14:29:40 -0500

    don't reset settings when extension updated

--- src/background.ts
+++ src/background.ts
@@ -6,23 +6,28 @@ type tabId=number;
 const VERSION='VERSION_INSERTED_HERE_BY_BUILD_SH';
 
 chrome.runtime.onInstalled.addListener(()=>{ //installed, so set default settings
-    chrome.storage.sync.set({
-        version: VERSION, //for debugging and backwards compatibility perhaps in the future
-        settings: {
-            fullScreen: true,
-            printLayout: false,
-            pomodoroEnabled: true,
-            showPageSeparators: false,
-            enterFocusModeOnTimerStart: false,
-            exitFocusModeOnTimerEnd: false,
-            darkMode: false,
-            darkModeAmount: 0.92,
-            zoom: '1.15', //'0.85' - small, '1' - normal, '1.15' - large, '1.3' - extra large
-            breakDuration: 5, //in minutes
-            breaksEnabled: true,
-            showDocumentTabs: false
-        }
-    });
+    chrome.storage.sync.get('version')
+        .then(version=>{
+            if (version) return; //don't set settings if already set
+
+            chrome.storage.sync.set({
+                version: VERSION, //for debugging and backwards compatibility perhaps in the future
+                settings: {
+                    fullScreen: true,
+                    printLayout: false,
+                    pomodoroEnabled: true,
+                    showPageSeparators: false,
+                    enterFocusModeOnTimerStart: false,
+                    exitFocusModeOnTimerEnd: false,
+                    darkMode: false,
+                    darkModeAmount: 0.92,
+                    zoom: '1.15', //'0.85' - small, '1' - normal, '1.15' - large, '1.3' - extra large
+                    breakDuration: 5, //in minutes
+                    breaksEnabled: true,
+                    showDocumentTabs: false
+                }
+            });
+        });
 });
 
 chrome.commands.onCommand.addListener((command)=>{
