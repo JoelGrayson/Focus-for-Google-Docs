26b28e3b10d3083e17ffe8c697e221857ca4a8f2
Joel Grayson <joel@joelgrayson.com>
2024-12-28 16:00:54 -0500

    fill in default settings only when settings not defined

--- src/background.ts
+++ src/background.ts
@@ -6,26 +6,30 @@ type tabId=number;
 const VERSION='VERSION_INSERTED_HERE_BY_BUILD_SH';
 
 chrome.runtime.onInstalled.addListener(()=>{ //installed, so set default settings
-    chrome.storage.sync.get('version')
-        .then(version=>{
-            if (version) return; //don't set settings if already set
+    chrome.storage.sync.get('settings')
+        .then(({ settings })=>{
+            const defaultSettings={
+                fullScreen: true,
+                printLayout: false,
+                pomodoroEnabled: true,
+                showPageSeparators: false,
+                enterFocusModeOnTimerStart: false,
+                exitFocusModeOnTimerEnd: false,
+                darkMode: false,
+                darkModeAmount: 0.92,
+                zoom: '1.15', //'0.85' - small, '1' - normal, '1.15' - large, '1.3' - extra large
+                breakDuration: 5, //in minutes
+                breaksEnabled: true,
+                showDocumentTabs: false
+            };
+
+            for (const key in defaultSettings) //set 
+                if (settings[key]===undefined)
+                    settings[key]=defaultSettings[key];
 
             chrome.storage.sync.set({
                 version: VERSION, //for debugging and backwards compatibility perhaps in the future
-                settings: {
-                    fullScreen: true,
-                    printLayout: false,
-                    pomodoroEnabled: true,
-                    showPageSeparators: false,
-                    enterFocusModeOnTimerStart: false,
-                    exitFocusModeOnTimerEnd: false,
-                    darkMode: false,
-                    darkModeAmount: 0.92,
-                    zoom: '1.15', //'0.85' - small, '1' - normal, '1.15' - large, '1.3' - extra large
-                    breakDuration: 5, //in minutes
-                    breaksEnabled: true,
-                    showDocumentTabs: false
-                }
+                settings
             });
         });
 });
